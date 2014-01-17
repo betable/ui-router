4eaa2db3954b7d7d85a99f6daaa93f192122abb8
Kevin King <kevinking@Kevins-MacBook-Pro.local>
2014-01-17 00:12:57 -0800

    release 0.2.9

--- release/angular-ui-router.min.js
+++ release/angular-ui-router.min.js
@@ -1,6 +1,6 @@
 /**
  * State-based routing for AngularJS
- * @version v0.2.8-dev-2014-01-17
+ * @version v0.2.9-dev-2014-01-17
  * @link http://angular-ui.github.com/
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
